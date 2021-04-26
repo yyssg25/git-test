f7a9fc516f7d84e52fa5f04d4c927950f6d0bb33
yy <1411993629@qq.com>
2021-04-26 20:16:31 +0800

    first commit

---
+++ src/main/java/com/yy/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.yy.git;
+
+/**
+ * @author yuye
+ * @Description This is description of method
+ * @Since version-1.0
+ * @date 2021-04-26 20:13
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
