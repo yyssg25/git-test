431a67fd09d403608f807a05984633bcf8995047
yy <1411993629@qq.com>
2021-04-26 20:20:06 +0800

    second commit

--- src/main/java/com/yy/git/GitTest.java
+++ src/main/java/com/yy/git/GitTest.java
@@ -10,5 +10,6 @@ package com.yy.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
